519da86478f5400e240beb31bdeecbe74285edab
lilieming <lilieming@xiaoduotech.com>
2018-07-27 16:53:05 +0800

    1. 封装fetch 2. github oAuth开发

--- src/server/githubAuth.js
+++ src/server/githubAuth.js
@@ -3,7 +3,7 @@
  * Date 2018/7/27
  * Time 13:07
  */
-const { getData, postData } = require('../shared/fetch')
+const { doFetch } = require('../shared/fetch')
 const config = require('./privateConfig')
 const Router = require('koa-router')
 
@@ -11,12 +11,14 @@ function init(app) {
   const router = new Router()
   router.get('/xhr/oauth/github/codeCallback', async (ctx, next) => {
     const code = ctx.query.code
-    const token = await postData('https://github.com/login/oauth/access_token', {
-      data: JSON.stringify({
-        client_id: config.clientId,
-        client_secret: config.clientSecret,
+    const token = await doFetch('https://github.com/login/oauth/access_token', {
+      data: {
+        client_id: config.githubOauth.clientId,
+        client_secret: config.githubOauth.clientSecret,
         code,
-      })
+      },
+      method: 'post',
+      contentType: 'form',
     })
     console.log('github oauth success', token)
     ctx.body = token
--- src/shared/fetch.js
+++ src/shared/fetch.js
@@ -4,9 +4,12 @@
  * Time 13:11
  */
 require('isomorphic-fetch')
+const qs = require('qs')
 
 /*
 *
+*  fetchOptions
+*   {
 *   body: JSON.stringify(data), // must match 'Content-Type' header
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     credentials: 'same-origin', // include, same-origin, *omit
@@ -18,21 +21,76 @@ require('isomorphic-fetch')
     mode: 'cors', // no-cors, cors, *same-origin
     redirect: 'follow', // manual, *follow, error
     referrer: 'no-referrer', // *client, no-referrer
+    }
 * */
 
-function fetchJson(url, options = {}) {
-  options.headers = options.headers || {}
-  options.headers = Object.assign({
+const fetchOptionsDefault = {
+  cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+  credentials: 'same-origin', // include, same-origin, *omit
+  headers: {
     Accept: 'application/json',
-  }, options.headers)
-  return fetch(url, options).then(response => response.json())
+    'Content-Type': 'application/json'
+  },
+  method: 'GET', // *GET, POST, PUT, DELETE, etc.
+  mode: 'cors', // no-cors, cors, *same-origin
+  redirect: 'follow', // manual, *follow, error
 }
-function postData(url, options = {}) {
-  return fetchJson(url, Object.assign({}, options,{method: 'post'}))
+
+const optionsDefault = {
+  data: null,
+  method: 'get',
+  contentType: 'json', // json form multipart
+  accept: 'json', // json or other
+  headers: {},
 }
-function getData(url, options = {}) {
-  return fetchJson(url, Object.assign({}, options,{method: 'get'}))
+function doFetch(url, options = {}) {
+  options = Object.assign({}, optionsDefault, options)
+  const fetchOptions = Object.assign({}, fetchOptionsDefault)
+  fetchOptions.method = options.method.toUpperCase()
+
+  if (options.headers) {
+    Object.assign(fetchOptions, options.headers )
+  }
+  // 'application/json, application/xml, text/plain, text/html, */*'
+  if (options.accept && options.accept !== 'json') {
+    fetchOptions.headers.Accept = options.accept
+  }
+
+  if (options.contentType && options.contentType !== 'json') {
+    switch(options.contentType) {
+      case 'form':
+        fetchOptions.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+        fetchOptions.body = qs.stringify(options.data || {})
+        break
+      case 'multipart':
+        fetchOptions.headers['Content-Type'] = 'multipart/form-data'
+        fetchOptions.body = options.data // formData
+        break
+      default :
+        fetchOptions.headers['Content-Type'] = options.contentType
+        fetchOptions.body = options.data
+        break
+    }
+  } else {
+    fetchOptions.body = JSON.stringify(options.data || {})
+  }
+  if (options.method === 'get') {
+    fetchOptions.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    if (options.data) {
+      url += url.indexOf('?') >= 0 ? '&' : '?'
+      url += qs.stringify(options.data || {})
+      delete options.data
+    }
+  }
+  const req = fetch(url, fetchOptions)
+  console.log(url, fetchOptions)
+  if (options.accept === 'json') {
+    return req.then(response => response.json())
+  } else {
+    return req
+  }
 }
 module.exports = {
-  fetchJson, getData, postData,
+  doFetch
 }
+
